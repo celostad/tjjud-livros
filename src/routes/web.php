61f0a6cde15af066a174f906fd5cb2f963a95153
Marcelo de Souza Tadim Brasileiro <marcelo.brasileiro@previdencia.gov.br>
2026-06-29 18:14:44 -0300

    fix - ajusta edição de forms

--- src/routes/web.php
+++ src/routes/web.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => redirect()->route('livros.index'))->name('home');
 Route::resource('livros', LivroController::class);
-Route::resource('autores', AutorController::class);
+Route::resource('autores', AutorController::class)
+    ->parameters([
+        'autores' => 'autor',
+    ]);
+
 Route::resource('assuntos', AssuntoController::class);
 
 Route::prefix('relatorio')->name('relatorio.')->group(function () {
